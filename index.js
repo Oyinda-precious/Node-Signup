f59a3a8a0185c3daef0a566f89b9a846790beed5
Oyinda-Precious <ayoajiboyeoyin@gmail.com>
2024-09-12 08:32:25 +0200

    notdone

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 let userarray = [];
+let todoarray = [];
 let errormessage = "";
 
 app.get("/", (request, response) => {
@@ -17,6 +18,10 @@ app.get("/login", (req, res) => {
 
 app.get("/dashboard", (req, res) => {
   res.render("dashboard");
+});
+
+app.get("/todo", (req, res) => {
+  res.render("todo", { todoarray });
 });
 
 app.post("/register", (req, res) => {
@@ -36,10 +41,37 @@ app.post("/register", (req, res) => {
 
 app.post("/onlinelogin", (req, res) => {
   console.log(req.body);
-  let body = req.body;
-  userarray.push(body);
-  console.log(userarray);
-  res.redirect("/dashboard");
+  // /this is to have access to what user is sending
+  // let body = req.body;
+  const existuser = userarray.find((user) => user.email == req.body.email);
+  console.log(existuser);
+  if (!existuser) {
+    console.log("you are not a registered user ; please sign up");
+  } else {
+    if (existuser.password == req.body.password) {
+      console.log("login successful");
+      res.redirect("/dashboard");
+    } else {
+      console.log("invalid password");
+    }
+  }
+});
+app.post("/addtodo", (req, res) => {
+  console.log(req.body);
+  todoarray.push(req.body);
+  res.redirect("/todo");
+});
+
+app.post("/delete", (req, res) => {
+  console.log(req.body);
+  todoarray.splice(req.body, 1);
+  res.redirect("/todo");
+});
+
+app.post("/edit", (req, res) => {
+  console.log(req.body);
+
+  res.redirect("/todo");
 });
 
 const port = 8003;
